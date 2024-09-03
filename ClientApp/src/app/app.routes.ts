257fc0839f90a5a4510f53368740daeded424258
Malek <mooabdelmalek@gmail.com>
2024-09-04 00:21:26 +0300

    The implement for front end on back end

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
 import { AuthGuard } from './shared/guards/authorization.guard';
 import { PlayModule } from './Play/play.module';
-import { VendorGuard } from './shared/guards/vendor.guard';
 import { CustomerGuard } from './shared/guards/customer.guard';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 export const routes: Routes = [
     {path:"" ,component:HomeComponent},
+    {path:"about" , component:AboutComponent},
+    {path:"contact" , component:ContactComponent},
     {path:"",
         canActivate:[AuthGuard , CustomerGuard],
         children:[{
